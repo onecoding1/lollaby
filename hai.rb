9ff2b1c568ae75c8a42a90c56e87ccde38a560ce
Fyskij <fyskij@gmail.com>
2010-07-06 13:23:05 +0200

    heroku sucks

--- hai.rb
+++ hai.rb
@@ -12,7 +12,7 @@ DataMapper.finalize
 
 class Hai < Sinatra::Base
 
-  db = "sqlite://#{ENV["PWD"]}/quotes.db"
+  db = "sqlite://quotes.db"
   DataMapper.setup(:default, db.to_s)
 
   set :views, File.dirname(__FILE__) + '/templates'
